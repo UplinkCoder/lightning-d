21054926c508128302ecaf27016be62d7711a344
Stefan Koch <uplink.coder@googlemail.com>
2021-07-05 14:16:20 +0200

    Import archcitecture specific code in version block

--- printf_example.d
+++ printf_example.d
@@ -1,6 +1,19 @@
 import core.stdc.stdio;
 import lightning;
-import jit_x86;
+
+version(AArch64)
+{
+    import jit_aarch64;
+}
+else version (X86_64)
+{
+    import jit_x86;
+}
+else version (X86)
+{
+    import jit_x86;
+}
+else static assert(0, "Architecture unsupported");
 
 static jit_state_t* _jit;
 
@@ -37,5 +50,43 @@ int main(string[] args)
 
   _jit_destroy_state(_jit);
   finish_jit();
+
+  printf("54 + 102 = %d\n", expensiveAdd(54, 102));
+  printf("54 + (-30) = %d\n", expensiveAdd(54, -30));
+
   return 0;
+}
+
+int expensiveAdd(int a, int b)
+{
+  int function (int, int) add;
+
+  init_jit(null);
+  _jit = jit_new_state();
+
+  jit_node_t*    arg1, arg2;      /* to get the argument */
+
+  _jit_prolog(_jit);
+  arg1 = _jit_arg(_jit);
+  arg2 = _jit_arg(_jit);
+
+  _jit_getarg_i(_jit, JIT_R1, arg1);
+  _jit_getarg_i(_jit, JIT_R2, arg2);
+  _jit_prepare(_jit);
+
+  _jit_new_node_www(_jit, jit_code_t.jit_code_addr, JIT_R0, JIT_R1, JIT_R2);
+
+  _jit_retr(_jit, JIT_R0);
+  _jit_epilog(_jit);
+
+  add = cast(typeof(add))_jit_emit(_jit);
+
+  /* call the generated code, passing its size as argument */
+  auto result = add(a, b);
+  _jit_clear_state(_jit);
+
+  _jit_destroy_state(_jit);
+  finish_jit();
+
+  return result;
 }
